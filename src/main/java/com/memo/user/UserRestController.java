c593fd560c0e6b288402390740fad62a107276cf
test <test@test.com>
2024-01-12 18:42:44 +0900

    메모

---
+++ src/main/java/com/memo/user/UserRestController.java
@@ -0,0 +1,24 @@
+package com.memo.user;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/user")
+@RestController
+public class UserRestController {
+	
+	@RequestMapping("/is-duplicated-id")
+	public Map<String, Object> isDuplicatedId(
+			@RequestParam("loginId") String loginId) {
+		// DB 조회 - select
+		
+		Map<String, Object> result = new HashMap<>();
+		result.put("code", 200);
+		result.put("is_duplicated_id", true);
+		return result;
+	}
+}
