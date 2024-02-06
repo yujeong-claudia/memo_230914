4304650a2f6e5b31bb89ef39ac24b028ac04987e
test <test@test.com>
2024-02-06 17:18:04 +0900

    JUnit test 비어있는지 확인 테스트

--- src/test/java/com/memo/MemoApplicationTests.java
+++ src/test/java/com/memo/MemoApplicationTests.java
@@ -2,7 +2,11 @@ package com.memo;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.util.ObjectUtils;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -14,12 +18,27 @@ class MemoApplicationTests {
 //	void contextLoads() {
 //	}
 	
-	@Test
+	//@Test
 	void 더하기테스트() {
 		log.info("$$$$$$ 더하기 테스트 시작");
 		int a = 10;
 		int b = 20;
 		
 		assertEquals(20, a + b);
+	}
+	
+	@Test
+	void 비어있는지확인() {
+		//List<Integer> list = new ArrayList<>(); // []
+		List<Integer> list = null;
+		if (ObjectUtils.isEmpty(list)) {
+			log.info("list is empty.");
+		}
+		
+		//String str = null;
+		String str = "";
+		if (ObjectUtils.isEmpty(str)) {
+			log.info("str is empty.");
+		}
 	}
 }
